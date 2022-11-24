57c17d6fdf06d7e0e9f42bbd2282037faae63dca
carl-lagerlof <carl.lagerlof.gbg22@amerikanskagymnasiet.se>
2022-11-24 20:37:16 +0100

    Commented on all of the necissary comments

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,4 @@
+// Här lägger jag till alla bilder som jag vill ha i min slideshow
 var imgArray = [
     './img/cola.png',
     './img/colavanilla.png',
@@ -6,20 +7,21 @@ var imgArray = [
     './img/fantaexotic.png'
 
 ];
-var curIndex = 0;
 
-var imgDuration = 2000;
+var curIndex = 0;
+// Här bestämmer jag hur länge varje bild ska visas innan det byts
+var imgDuration = 1800;
 
 function slideShow() {
-    
+    // Här sätter jag så att ID image1 sätts på positionen som curIndex säger
     document.getElementById('image1').src = imgArray[curIndex];
     curIndex++;
     
-
+    // Detta gör så att slideshowen börjar om efter den har gått igenom alla bilder
     if (curIndex == imgArray.length) { curIndex = 0; }
     
-
+    // Denna är det som får en ny bild komma fram efter tiden satt i imgDuration
     setTimeout("slideShow()", imgDuration);
 }
-
+// Påbörjar slideshowen
 slideShow();
